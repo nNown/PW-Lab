6bcf20ca8c22d070784d17d57853f8a74e4593a9
Wojciech Marciniak <wojciechmar2000@gmail.com>
2021-10-11 14:53:21 +0200

    Uploaded code

---
+++ PW-Lab-WMarciniak/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace PW_Lab_WMarciniak
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            time.Content = DateTime.Now.ToLongTimeString();
+            stoper.Click += openStoper;
+
+            DispatcherTimer dispachterTimer = new DispatcherTimer();
+            dispachterTimer.Tick += updateTime;
+            dispachterTimer.Interval = new TimeSpan(0, 0, 1);
+            dispachterTimer.Start();
+        }
+
+        private void updateTime(object sender, EventArgs e)
+        {
+            time.Content = DateTime.Now.ToLongTimeString();
+        }
+
+        private void openStoper(object sender, EventArgs e)
+        {
+            Stoper stoper = new Stoper();
+            stoper.Show();
+        }
+    }
+}
---
+++ PW-Lab-WMarciniak/Stoper.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace PW_Lab_WMarciniak
+{
+    /// <summary>
+    /// Interaction logic for Stoper.xaml
+    /// </summary>
+    public partial class Stoper : Window
+    {
+        private DispatcherTimer _stoperDispatcher;
+        private TimeSpan _time;
+        public Stoper()
+        {
+            InitializeComponent();
+            stoper.Content = _time.Duration().ToString();
+
+            _stoperDispatcher = new DispatcherTimer();
+            _stoperDispatcher.Tick += updateTime;
+            _stoperDispatcher.Interval = new TimeSpan(TimeSpan.TicksPerMillisecond);
+
+            start.Click += startStoper;
+            stop.Click += (o, e) => _stoperDispatcher.Stop();
+        }
+
+        private void updateTime(object sender, EventArgs e)
+        {
+            _time += new TimeSpan(TimeSpan.TicksPerMillisecond);
+            stoper.Content = _time.Duration().ToString();
+        }
+
+        private void startStoper(object sender, EventArgs e)
+        {
+            _time = new TimeSpan();
+            stoper.Content = _time.Duration().ToString();
+            _stoperDispatcher.Start();
+        }
+    }
+}
