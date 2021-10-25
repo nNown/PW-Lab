8c42985f1185e619dc0f55280407b8c975f8910c
Wojciech Marciniak <wojciechmar2000@gmail.com>
2021-10-25 14:57:22 +0200

    Add files via upload

---
+++ PW-Lab3/Row.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PW_Lab3
+{
+    public class Row
+    {
+        public string Name { get; set; }
+        
+        public string Id { get; set; }
+        
+        public int Count { get; set; }
+    }
+}
