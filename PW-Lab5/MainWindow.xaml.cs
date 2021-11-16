34185d05ec3cac1d366c332a6cd3b719a15b77b2
nNown <wojciechmar2000@gmail.com>
2021-11-16 19:56:38 +0100

    Created pattern finder

---
+++ PW-Lab5/MainWindow.xaml.cs
@@ -0,0 +1,112 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PW_Lab5
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private string _sequence;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public void LoadSequence(object sender, EventArgs args) {
+            var dialog = new OpenFileDialog();
+            dialog.Filter = "TXT|*.txt";
+            dialog.Title = "Open a sequence file";
+
+            if(dialog.ShowDialog() ?? false)
+            {
+                _sequence = File.ReadAllText(dialog.FileName);
+                UpdateSequenceTextBox(_sequence);
+            }
+        }
+
+        public void FindPatterns(object sender, EventArgs args)
+        {
+            var tags = PatternFinder.FindPatterns(_sequence, 4);
+            foreach(var tag in tags)
+            {
+                Tags.Text += $"{tag.Key} {tag.Value}\n";
+            }
+            UpdateDropDownMenu(tags.Keys);
+        }
+
+        public void SelectPattern(object sender, EventArgs args)
+            => SelectTag(DropDown.SelectedItem.ToString());
+
+        private void UpdateSequenceTextBox(string sequence) 
+            => Sequence.Document.Blocks.Add(new Paragraph(new Run(sequence)));
+
+        private void UpdateDropDownMenu(IEnumerable<string> tags)
+        {
+            foreach(var tag in tags)
+            {
+                DropDown.Items.Add(tag);
+            }
+
+            DropDown.SelectedItem = DropDown.Items[0];
+            SelectTag(DropDown.SelectedItem.ToString());
+        }
+
+        private void SelectTag(string tag)
+        {
+            new TextRange(Sequence.Document.ContentStart, Sequence.Document.ContentEnd).ApplyPropertyValue(TextElement.ForegroundProperty, Brushes.Black);
+
+            var tagRanges = GetAllTagRanges(Sequence.Document, tag);
+            foreach(var tagRange in tagRanges)
+            {
+                if(tagRange.Text == tag)
+                {
+                    tagRange.ApplyPropertyValue(TextElement.ForegroundProperty, Brushes.BlueViolet);
+                }
+            }
+        }
+
+        private IEnumerable<TextRange> GetAllTagRanges(FlowDocument document, string pattern)
+        {
+            TextPointer pointer = document.ContentStart;
+            while(pointer != null)
+            {
+                if(pointer.GetPointerContext(LogicalDirection.Forward) == TextPointerContext.Text)
+                {
+                    string textRun = pointer.GetTextInRun(LogicalDirection.Forward);
+                    MatchCollection matches = Regex.Matches(textRun, pattern);
+                    foreach (Match match in matches)
+                    {
+                        int startIndex = match.Index;
+                        int length = match.Length;
+                        TextPointer start = pointer.GetPositionAtOffset(startIndex);
+                        TextPointer end = pointer.GetPositionAtOffset(startIndex + length);
+                        yield return new TextRange(start, end);
+                    }
+                }
+
+                pointer = pointer.GetNextContextPosition(LogicalDirection.Forward);
+            }
+        }
+    }
+}
---
+++ PW-Lab5/PatternFinder.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PW_Lab5
+{
+    public static class PatternFinder
+    {
+        public static Dictionary<string, int> FindPatterns(string sequence, int length)
+        {
+            var patterns = new Dictionary<string, int>();
+
+            foreach(var tag in AllTags(sequence, length))
+            {
+                if(!patterns.ContainsKey(tag))
+                {
+                    patterns.Add(tag, 0);
+                }
+
+                patterns[tag] += 1;
+            }
+
+            return patterns;
+        }
+
+        private static string Tag(string sequence, int start, int end)
+        {
+            var tag = string.Empty;
+            for(int i = start; i < end; i++)
+            {
+                tag += sequence[i];
+            }
+            return tag;
+        }
+
+        private static IEnumerable<string> AllTags(string sequence, int length)
+        {
+            for(int i = 0; i < sequence.Length - length; i++)
+            {
+                yield return Tag(sequence, i, i + length);
+            }
+        }
+    }
+}
