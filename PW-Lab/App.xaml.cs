a182c7ca9a0beee553a5c0949b4505f80aabe914
nNown <wojciechmar2000@gmail.com>
2022-01-03 15:03:38 +0100

    Finished project

--- PW-Lab/App.xaml.cs
+++ PW-Lab/App.xaml.cs
@@ -1,7 +1,9 @@
-﻿using PW_Lab.Model;
-using PW_Lab.View;
-using PW_Lab.ViewModel;
-using System.Collections.ObjectModel;
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
 using System.Windows;
 
 namespace PW_Lab
@@ -11,12 +13,5 @@ namespace PW_Lab
     /// </summary>
     public partial class App : Application
     {
-        protected override void OnStartup(StartupEventArgs e)
-        {
-            base.OnStartup(e);
-            var formViewModel = new FormViewModel() { Students = new ObservableCollection<Student>() { new Student() { NameAndSurname = "", DateAndSignature = "", Id = 0 } } };
-            var window = new MainWindow(formViewModel);
-            window.Show();
-        }
     }
 }
---
+++ PW-Lab/MainWindow.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Windows;
+using System.Windows.Media;
+
+namespace PW_Lab
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private MediaPlayer _mediaPlayer = new MediaPlayer();
+
+        public ObservableCollection<string> MusicFiles { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            string[] files = Directory.GetFiles(Directory.GetCurrentDirectory() + "\\songs");
+            MusicFiles = new ObservableCollection<string>(files);
+            DataContext = this;
+        }
+
+        private void PlaySong(object sender, EventArgs args)
+        {
+            _mediaPlayer.Open(new Uri((string)MusicList.SelectedItem));
+            _mediaPlayer.Play();
+        }
+
+        private void StopSong(object sender, EventArgs args)
+        {
+            _mediaPlayer.Stop();
+        }
+    }
+}
